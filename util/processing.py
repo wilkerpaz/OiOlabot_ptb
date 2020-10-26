d61a236c07dc2cb31349cd1f4fe13b65b0903d0d
wilkerpaz <wilkerpaz@hotmail.com>
2020-10-26 18:15:00 -0300

    update bot

--- util/processing.py
+++ util/processing.py
@@ -52,7 +52,7 @@ class BatchProcess(threading.Thread):
                 get_url_info = self.db.get_update_url(url)
                 last_url = get_url_info['last_url']
                 date_last_url = DateHandler.parse_datetime(get_url_info['last_update'])
-                feed = FeedHandler.parse_feed(url, 4, date_last_url)
+                feed = FeedHandler.parse_feed(url, 4, date_last_url + DateHandler.datetime.timedelta(days=-1))
                 for post in feed:
                     if not hasattr(post, "published") and not hasattr(post, "daily_liturgy"):
                         logger.warning('not published' + url)
